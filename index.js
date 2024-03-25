27375aa1a2a478558bbf06fff19dfee0d2e7099a
Ashley Manese <ashleymanese31@gmail.com>
2024-03-24 19:21:38 -0700

    add google log in

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import env from "dotenv";
+import GoogleStrategy from "passport-google-oauth2";
 
 const app = express();
 const port = 3000;
@@ -103,6 +104,28 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
+
+app.get("/logout", (req,res) => {
+  req.logout((err) => {
+    if(err) console.log(err)
+    res.redirect("/")
+  })
+})
+
 app.post(
   "/login",
   passport.authenticate("local", {
@@ -144,6 +167,37 @@ passport.use(
     //if match send to secrets page
     //if not tell them no match
   })
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+      try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [
+          profile.email,
+        ]);
+        if (result.rows.length === 0) {
+          await db.query("INSERT INTO users (email, password) VALUES ($1,$2)", [
+            profile.email,
+            "google",
+          ]);
+          cb(null, user);
+        }else{
+          cb(null, result.rows[0])
+        }
+      } catch (error) {
+        cb(error)
+      }
+    }
+  )
 );
 
 // save data of logged in user to local storage
